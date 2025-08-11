856747b382dde846e68c647e310654954117084a
sonir746 <sonir7469@gmail.com>
2025-08-11 17:43:12 +0530

    update static in stating.py

--- PC_Verification/settings.py
+++ PC_Verification/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,6 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "verification" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
